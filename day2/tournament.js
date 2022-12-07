351bd346156be431b65feda1554f507f8d2e6b0b
stephaniesomade <stephaniesomadee@gmail.com>
2022-12-07 13:02:48 +0000

    Day 2 Part 2 Completed

--- day2/tournament.js
+++ day2/tournament.js
@@ -4,58 +4,88 @@ const input = fs.readFileSync('day2.txt', 'utf8').trimEnd();
 class Tournament { 
   constructor() {
     this.input = input;
-    this.arr = [];
-    this.score = 0;
+    this.firstScore = 0;
+    this.secondScore = 0;
   }
-
-  results() { 
+  firstResults() { 
     const array = this.input.split("\n") 
-    // Yourmove is Rock, Rock/A - Draw(3), Paper/B - Lose (0), Scissors/C - Win(6)
     array.forEach((moves) => { 
       let yourMove = moves[2];
       let theirMove = moves[0];
       if (yourMove === "X") { 
-        this.score += 1;
+        this.firstScore += 1;
         if (theirMove === "A") {
-          this.score += 3;
+          this.firstScore += 3;
         } else if (theirMove === "B"){
-          this.score += 0;
+          this.firstScore += 0;
         } else {
-          this.score += 6;
+          this.firstScore += 6;
         }
       }
-      // Yourmove is Paper, Rock/A - Win (6), Paper/B - Draw (3), Scissors/C - Lose (0)
       else if (yourMove === "Y") { 
-        this.score += 2;
+        this.firstScore += 2;
         if (theirMove === "A") { 
-          this.score += 6;
+          this.firstScore += 6;
         } else if (theirMove === "B") { 
-          this.score += 3;
+          this.firstScore += 3;
         } else { 
-          this.score += 0;
+          this.firstScore += 0;
         }
       }
-
-      // YourMove is Scissors, Rock/A - Lose (0), Paper/B - Win(6), Scissors/C - Draw(3)
       else if (yourMove === "Z") {
-        this.score += 3;
+        this.firstScore += 3;
         if (theirMove === "A") { 
-          this.score += 0;
+          this.firstScore += 0;
         } else if (theirMove === "B") { 
-          this.score += 6;
+          this.firstScore += 6;
         } else {
-          this.score += 3;
+          this.firstScore += 3;
         }
       }
       else {
         console.log("error")
       }
     })
-    console.log(this.score)
+    console.log(this.firstScore)
+  }
+
+  secondResults() { 
+    const array = this.input.split("\n") 
+    array.forEach((moves) => {
+    let result = moves[2];
+    let theirMove = moves[0];
+    if (theirMove === "A") {
+      if (result === "X") { 
+        this.secondScore += 3
+      } else if (result === "Y") { 
+        this.secondScore += 4
+      } else {
+        this.secondScore += 8
+      }
+      } else if (theirMove === "B") {
+        if (result === "X") { 
+          this.secondScore += 1
+        } else if (result === "Y") { 
+          this.secondScore += 5
+        } else { 
+          this.secondScore += 9
+        }
+      } else { 
+        if (result === "X") { 
+          this.secondScore += 2
+        } else if (result === "Y") {
+          this.secondScore += 6
+        } else { 
+          this.secondScore += 7
+        }
+      }
+    })
+    console.log(this.secondScore)
   }
   
 }
 
 const game = new Tournament;
-game.results();
-// can be refactored
+// game.firstResults();
+game.secondResults();
+// will be refactored
